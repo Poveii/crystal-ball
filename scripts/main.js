63d59303d421d676be7687c3e3a4d03baa415267
Poveii <yellowtutors@gmail.com>
2022-07-20 09:42:29 -0300

    Learning about favicons

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 /* ========== VARIABLES ========== */
 const elementPhrase = document.querySelector("#phrase");
 const phrasesQuestions = [
-  "Vou revelar o seu destino!",
-  "O que você vai me perguntar agora?",
-  "Tem algo em mente? Faça uma pergunta",
-  "Eu não tenho o dia todo, faça logo a sua pergunta",
-  "A certeza não é uma certeza de uma certeza",
+  "I will reveal your destiny!",
+  "What are you going to ask me now?",
+  "Have something in mind? Make a question",
+  "I don't have all day, just make your question",
+  "I'm waiting for you ask me...",
 ];
 
 /* ========== FUNCTIONS ========== */
