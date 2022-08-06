c0afa0900a4f1b7ad4210ac9267b967a992e3c24
Poveii <yellowtutors@gmail.com>
2022-08-06 17:34:50 -0300

    Refactoring variables names

--- scripts/answersCrystal.js
+++ scripts/answersCrystal.js
@@ -1,9 +1,9 @@
 /* ========= VARIABLES ========= */
-const elementAnswer = document.querySelector("#answer");
-const inputQuestion = document.querySelector("#inputQuestion");
-const buttonAsk = document.querySelector("#buttonAsk");
+const answerElement = document.querySelector("#answer");
+const inputQuestionElement = document.querySelector("#inputQuestion");
+const buttonAskElement = document.querySelector("#buttonAsk");
 
-const answersQuestions = [
+const answersToQuestions = [
   "Of Course!",
   "I'm not so sure.",
   "It's decidedly so.",
@@ -26,32 +26,13 @@ const answersQuestions = [
   "No.",
 ];
 
-const answersEmptyQuestions = [
+const answersToEmptyQuestions = [
   "You don't ask me anything...",
   "Make me a question, come on!",
   "Seriously? A question please.",
 ];
 
 /* ========= FUNCTIONS ========= */
-function verifyEmptyQuestion() {
-  if (inputQuestion.value == "") {
-    buttonAsk.setAttribute("disabled", true);
-
-    elementAnswer.style.opacity = 1;
-
-    let anyNumberAnswers = generateRandomNumbers(answersEmptyQuestions);
-
-    elementAnswer.innerHTML = answersEmptyQuestions[anyNumberAnswers];
-
-    setTimeout(function () {
-      elementAnswer.style.opacity = 0;
-      buttonAsk.removeAttribute("disabled");
-    }, 2000); // disappear answer after 2 seconds
-
-    return true;
-  } else return false;
-}
-
 function generateRandomNumbers(list) {
   const total = list.length;
   const randomNumber = Math.floor(Math.random() * total);
@@ -59,40 +40,62 @@ function generateRandomNumbers(list) {
 }
 
 function includesEasterEgg(easterEgg) {
-  if (inputQuestion.value.includes(easterEgg)) {
+  if (inputQuestionElement.value.includes(easterEgg)) {
+    return true;
+  } else return false;
+}
+
+function verifyEmptyQuestion() {
+  if (inputQuestionElement.value === "") {
+    buttonAskElement.setAttribute("disabled", true);
+
+    answerElement.style.opacity = 1;
+
+    let anyNumberToEmptyAnswers = generateRandomNumbers(
+      answersToEmptyQuestions
+    );
+
+    answerElement.innerHTML = answersToEmptyQuestions[anyNumberToEmptyAnswers];
+
+    setTimeout(function () {
+      answerElement.style.opacity = 0;
+      buttonAskElement.removeAttribute("disabled");
+    }, 2000); // disappear answer after 2 seconds
+
     return true;
   } else return false;
 }
 
 function askQuestion() {
-  if (verifyEmptyQuestion() == false) {
-    buttonAsk.setAttribute("disabled", true);
+  if (verifyEmptyQuestion() === false) {
+    buttonAskElement.setAttribute("disabled", true);
 
-    const question = "<div>" + inputQuestion.value + "</div>";
+    const question = "<div>" + inputQuestionElement.value + "</div>";
 
-    let anyNumberAnswers = generateRandomNumbers(answersQuestions);
+    let anyNumberToAnswers = generateRandomNumbers(answersToQuestions);
 
-    if (includesEasterEgg("can") == true) {
-      elementAnswer.innerHTML = "Everything will be alright!";
+    if (includesEasterEgg("can") === true) {
+      answerElement.innerHTML = "Everything will be alright!";
 
-      elementAnswer.style.opacity = 1;
+      answerElement.style.opacity = 1;
 
-      inputQuestion.value = "";
+      inputQuestionElement.value = "";
 
       setTimeout(function () {
-        elementAnswer.style.opacity = 0;
-        buttonAsk.removeAttribute("disabled");
+        answerElement.style.opacity = 0;
+        buttonAskElement.removeAttribute("disabled");
       }, 4000); // disappear answer after 4 seconds
     } else {
-      elementAnswer.innerHTML = question + answersQuestions[anyNumberAnswers];
+      answerElement.innerHTML =
+        question + answersToQuestions[anyNumberToAnswers];
 
-      elementAnswer.style.opacity = 1;
+      answerElement.style.opacity = 1;
 
-      inputQuestion.value = "";
+      inputQuestionElement.value = "";
 
       setTimeout(function () {
-        elementAnswer.style.opacity = 0;
-        buttonAsk.removeAttribute("disabled");
+        answerElement.style.opacity = 0;
+        buttonAskElement.removeAttribute("disabled");
       }, 3000); // disappear answer after 3 seconds
     }
   }
